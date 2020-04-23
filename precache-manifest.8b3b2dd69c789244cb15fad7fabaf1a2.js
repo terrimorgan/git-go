265c58984a681e3338bc2f169c64834e1be3389f
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2020-04-23 17:03:25 -0400

    Updates

---
+++ precache-manifest.8b3b2dd69c789244cb15fad7fabaf1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e5e2591884486dd9a5ed9d2d6f3117",
+    "url": "/git-go/index.html"
+  },
+  {
+    "revision": "3e29e4d8b7a2aaa11e2f",
+    "url": "/git-go/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d16a20f16b66feddaefd",
+    "url": "/git-go/static/js/2.fa628831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-go/static/js/2.fa628831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e29e4d8b7a2aaa11e2f",
+    "url": "/git-go/static/js/main.405f8339.chunk.js"
+  },
+  {
+    "revision": "0ddd8832501de3ceacb2",
+    "url": "/git-go/static/js/runtime-main.b15d12f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-go/static/media/logo.5d5d9eef.svg"
+  }
+]);
