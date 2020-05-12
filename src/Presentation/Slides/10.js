dd60f3664e1cead7627dc6ede37a9b4641327677
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2020-05-12 19:16:07 -0400

    Add sign-in sheet

---
+++ src/Presentation/Slides/10.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Participant from '../Participant'
+
+const participants = [
+  'Wilfredo Velázquez-Rodríguez'
+];
+
+export default
+<div className="each-fade">
+  <h2>Sign-in Sheet</h2>
+  <p>
+    Add your name to the list to show you've been here!
+    <ul>
+      {participants.map(name => <li><Participant name={name} /></li>)}
+    </ul>
+  </p>
+</div>
